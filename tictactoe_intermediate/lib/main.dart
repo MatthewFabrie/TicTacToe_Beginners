69e7b6c9e61f092bbdeeb8888f3284573e76672e
Matthew <matthew.fabrie@epitech.eu>
2023-01-10 15:59:38 +0100

    adding main file for intermediate tictactoe

---
+++ tictactoe_intermediate/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+ 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+ 
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Good luck, if you have any questions or you need help you can check the beginner github or you can call me
+    );
+  }
+}
